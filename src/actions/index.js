cb8b8eb40abaf1f28ed58b186e6bf6e37cbc9a1d
Aniruddh Gopale <anigopale@gmail.com>
2017-10-02 16:42:41 +0530

    ajax request with axios, action creator created

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = 'f4b595963e21b9da054693a7ea3e58cc';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };// action creator, returns an action with type
+  // payload is data action carries
+}
